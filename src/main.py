75938b6dc202d5ff2592272d3222c05bec835f5c
Giovdi <fs.giordano@gmail.com>
2017-02-27 03:39:39 +0100

    small fix

--- src/main.py
+++ src/main.py
@@ -5,4 +5,4 @@ FontAwesome3To4.pathCheck("C:/path/to/folder", True)
 FontAwesome3To4.fileCheck("C:/path/to/file.php", True)
 # For Unix users
 FontAwesome3To4.pathCheck("/path/to/folder", True)
-FontAwesome3To4.pathCheck("/path/to/file.php", True)
+FontAwesome3To4.fileCheck("/path/to/file.php", True)
